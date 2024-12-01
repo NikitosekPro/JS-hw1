2f9ab52098b4c825a6df9f42e8031bc9fb3d4e30
NikitosekPro <148691930+NikitosekPro@users.noreply.github.com>
2024-12-01 14:25:31 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,34 +23,30 @@ console.log(myNull);
 
     const userName = prompt("Enter Your name.");
     console.log(userName);
-    if (userName != true) {
-        const hello = alert("Hello, " + userName);
+    if (userName) {
+        alert("Hello, " + userName);
     }
 
 
 
-const question = confirm("Would You like to subscribe to a newsletter?"); 
-console.log(question);
-if (question != false) {
-    const ok = alert("Thank's for subscribing to a newsletter!");
+const answer = confirm("Would You like to subscribe to a newsletter?"); 
+console.log(answer);
+if (answer) {
+    alert("Thank's for subscribing to a newsletter!");
 
 }
 
 else {
-    const ok = alert("Fine, but please subscribe next time.");
+    alert("Fine, but please subscribe next time.");
 }
 
 
-const danger = confirm("The file You're about to download can be dangerous for Your computer.  Would You like to download it anyway?");
-console.log(danger);
-if (danger != false) {
-    const download = alert("Downloading . . .")
-    console.log(download);
-    
-}
-
-else {
-    const fail = alert("You've just safed Your device from downloading dangerous virus. Be careful!");
-    console.log(fail);
+const dangerMessage = "The file You're about to download can be dangerous for Your computer.  Would You like to download it anyway?"
+const answerMessage = confirm(dangerMessage);
+console.log(answerMessage);
+if (answerMessage) {
+    alert("Downloading . . .");
+} else {
+    alert("You've just safed Your device from downloading dangerous virus. Be careful!");
 }
 
